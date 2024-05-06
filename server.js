9dd99cbd0c38c794acbab26858c2b92eb337c6f9
lmfontes19 <142419370+lmfontes19@users.noreply.github.com>
2024-05-05 19:22:07 -0600

    Enabling http server

--- server.js
+++ server.js
@@ -1,9 +1,12 @@
 const express = require("express");
 const fs = require("fs");
+const http = require("http");
 const router = require("./app/controllers/router");
+const dotenv = require('dotenv');
 
 let app = express();
-let puerto = 3000;
+
+dotenv.config();
 
 app.use(express.json());
 app.use(router);
@@ -13,7 +16,6 @@ app.use("/views_scripts", express.static("views"));
 app.use("/img", express.static("img"));
 app.use("/Styles", express.static("Styles"));
 
-
-app.listen(puerto, () => {
-    console.log("Practica 3 puerto:" + puerto);
-});
+const serverHttp = http.createServer(app);
+serverHttp.listen(process.env.HTTP_PORT, process.env.IP);
+serverHttp.on('listening', () => console.info(`Notes App running at http://${process.env.IP}:${process.env.HTTP_PORT}`));
